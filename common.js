002e9904d8f3154509c978d3127587f961078b34
fant <891650711@qq.com>
2021-04-10 10:52:30 +0800

    getTime

--- common.js
+++ common.js
@@ -62,5 +62,24 @@ export default {
               console.log('当前邮箱格式正确')
           }     
        }
+
+       //获取时间
+       Vue.prototype.getTime = function(){
+        var time=new Date();
+        var year=time.getFullYear();  //获取年份
+        var month=time.getMonth()+1;  //获取月份
+        var day=time.getDate();   //获取日期
+        var hour=checkTime(time.getHours());   //获取时
+        var minite=checkTime(time.getMinutes());  //获取分
+        var second=checkTime(time.getSeconds());  //获取秒
+        /****当时、分、秒、小于10时，则添加0****/
+        function checkTime(i){
+            if(i<10) return "0"+i;
+            return i;
+        }
+        const timer = year+"年"+month+"月"+day+"日"+hour+":"+minite+":"+second;
+        return timer
+       }
+
    }
 }
